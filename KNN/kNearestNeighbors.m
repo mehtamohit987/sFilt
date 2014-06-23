8c829bd1089c9c3c4d02b1133933e13b306d34b9
mehtamohit987 <mehtamohit987@gmail.com>
2014-06-23 14:08:05 +0530

    Create kNearestNeighbors.m

---
+++ KNN/kNearestNeighbors.m
@@ -0,0 +1,13 @@
+function [neighborIds neighborDistances] = kNearestNeighbors(dataMatrix, queryMatrix, k)
+
+neighborIds = zeros(size(queryMatrix,1),k);
+neighborDistances = neighborIds;
+
+numDataVectors = size(dataMatrix,1);
+numQueryVectors = size(queryMatrix,1);
+for i=1:numQueryVectors,
+    dist = sum((repmat(queryMatrix(i,:),numDataVectors,1)-dataMatrix).^2,2);
+    [sortval sortpos] = sort(dist,'ascend');
+    neighborIds(i,:) = sortpos(1:k);
+    neighborDistances(i,:) = sqrt(sortval(1:k));
+end
